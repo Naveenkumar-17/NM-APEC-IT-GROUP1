9fa945f271bdda12880721a17aad816529469482
Naveenkumar-17 <117798863+Naveenkumar-17@users.noreply.github.com>
2023-11-12 18:04:25 +0530

    Add files via upload

---
+++ BACKEND/SpringBoot_Backend/src/main/java/com/Yourpackage/service/TransactionService.java
@@ -0,0 +1,27 @@
+// TransactionService.java
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    // Other methods...
+}
